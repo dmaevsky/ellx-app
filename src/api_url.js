bbd0d9973f09ef33527a51c08deb7332a1c78fe2
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-01-31 09:33:58 +0900

    always fetch ~projects from prod env

--- src/api_url.js
+++ src/api_url.js
@@ -1,7 +1,4 @@
 // This is temporary
 // It should disappear once we get rid of "~owner/project" imports
 
-export default process.env.API_URL || (process.env.NODE_ENV === 'production'
-  ? 'https://api.ellx.io'
-  : 'https://test-api.ellx.io'
-);
+export default 'https://api.ellx.io';
