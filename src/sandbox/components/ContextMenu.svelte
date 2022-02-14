f60899c775030a43c5ea31283817b56bd13e339b
Andrei Marukhin <ax2mx@ya.ru>
2022-02-14 14:10:06 +0300

    Simplify context menu layout

--- src/sandbox/components/ContextMenu.svelte
+++ src/sandbox/components/ContextMenu.svelte
@@ -4,7 +4,7 @@
   import { getCoords, isMac } from "../../utils/ui.js";
   import { shortcuts } from "../../utils/shortcuts.js";
 
-  import MenuItem from './MenuItem.svelte';
+  import Shortcut from "./Shortcut.svelte";
 
   export let handleClipboard;
   export let container;
@@ -16,10 +16,6 @@
   let x, y;
   let menuNodes;
   let menuLength;
-
-  function getShortcutsByTag(str) {
-    return shortcuts.filter(i => i.tag.includes(str)).map(({ title, keys }) => [title, keys]);
-  }
 
   const rowHeight = 20, columnWidth = 100;
 
@@ -35,17 +31,6 @@
     menuNodes = [...menu.firstChild.children].filter(item => item.innerText !== ""); // Ignore dividers
     menuLength = menuNodes.length;
   });
-
-  function handleMouseHover(e) {
-    for (let i = 0; i < menuLength; i++) {
-      if (menuNodes[i] === e.target)  {
-        currentItem = i;
-        break;
-      }
-    }
-
-    e.target.focus();
-  }
 
   function dispatchSyntheticEvent(keys) {
     const event = new Event("keydown", {
@@ -76,6 +61,39 @@
     });
 
     container.dispatchEvent(event);
+  }
+
+  function getShortcutsByTag(str) {
+    return shortcuts.filter(i => i.tag.includes(str)).map(({ title, keys, tag }) => [title, keys, tag]);
+  }
+
+  function handleKeyDown(e) {
+    if (e.code === "Escape") return contextMenuOpen.set(false);
+
+    if (e.shiftKey) e.preventDefault(); // Prevent keyboard select
+
+    if (e.code === "ArrowDown" || e.code === "ArrowUp") {
+      if (e.code === "ArrowDown") currentItem = ++currentItem % menuLength;
+      else currentItem = (currentItem > 0) ? --currentItem : menuLength - 1;
+
+      menuNodes[currentItem].focus();
+    }
+  }
+
+  function handleMouseHover(e) {
+    for (let i = 0; i < menuLength; i++) {
+      if (menuNodes[i] === e.target)  {
+        currentItem = i;
+        break;
+      }
+    }
+
+    e.target.focus();
+  }
+
+  function handleMenuAction(title, keys, tag) {
+    if (tag.includes("clipboard")) handleClipboard(title);
+    else dispatchSyntheticEvent(keys);
   }
 
   function positionMenu(e) {
@@ -111,19 +129,6 @@
     menu.focus();
   }
 
-  function handleKeyDown(e) {
-    if (e.code === "Escape") return contextMenuOpen.set(false);
-
-    if (e.shiftKey) e.preventDefault(); // Prevent keyboard select
-
-    if (e.code === "ArrowDown" || e.code === "ArrowUp") {
-      if (e.code === "ArrowDown") currentItem = ++currentItem % menuLength;
-      else currentItem = (currentItem > 0) ? --currentItem : menuLength - 1;
-
-      menuNodes[currentItem].focus();
-    }
-  }
-
   $: if (event) {
     contextMenuOpen.set(true);
     currentItem = -1;
@@ -133,47 +138,31 @@
 
 </script>
 
-<div id="ellx-context-menu"
+<div id="ellx-context-menu" class="absolute z-50 font-sans py-2 rounded-sm bg-gray-100 text-gray-900 border border-gray-500 border-opacity-20 text-xs dark:bg-gray-900 dark:text-white focus:outline-none" style="left: {x}px; top: {y}px"
+  tabindex="-1"
   bind:this={menu}
   on:contextmenu={(e) => e.preventDefault()}
   on:mousedown={(e) => e.preventDefault()}
   on:click={() => contextMenuOpen.set(false)}
   on:keydown={handleKeyDown}
   class:hidden={!$contextMenuOpen}
-  tabindex="-1"
-  class="absolute z-50 font-sans py-2 rounded-sm bg-gray-100 text-gray-900 border border-gray-500 border-opacity-20 text-xs
-  dark:bg-gray-900 dark:text-white focus:outline-none" style="left: {x}px; top: {y}px"
 >
   <ul class="flex flex-col">
-    {#each menuItems as [title, keys, args]}
+    {#each menuItems as [title, keys, tag]}
       {#if title !== "-"}
-        {#if (title === "Copy" || title === "Cut" || title === "Paste")}
-          <MenuItem
-            {title} {keys}
-            onmousedown={() => handleClipboard(title)}
-            onmouseenter={handleMouseHover}
-            onkeydown={(e) => {
-              if (e.code === "Enter") {
-                handleClipboard(title);
-                contextMenuOpen.set(false);
-              }
-            }}
-          />
-        {:else}
-          <MenuItem
-            {title} {keys}
-            onmousedown={() => {
-              dispatchSyntheticEvent(keys)
-            }}
-            onmouseenter={handleMouseHover}
-            onkeydown={(e) => {
-              if (e.code === "Enter") {
-                dispatchSyntheticEvent(keys)
-                contextMenuOpen.set(false);
-              }
-            }}
-          />
-        {/if}
+        <li class="px-4 py-1 focus:bg-blue-600 focus:text-white focus:border-white focus:outline-none cursor-pointer capitalize"
+          tabindex="-1"
+          on:keydown={(e) => {
+            if (e.code === "Enter") {
+              handleMenuAction(title, keys, tag)
+              contextMenuOpen.set(false);
+            }
+          }}
+          on:mousedown={() => handleMenuAction(title, keys, tag)}
+          on:mouseenter={handleMouseHover}
+        >
+          <Shortcut {title} {keys}/>
+        </li>
       {:else}
         <hr class="my-2 opacity-60" />
       {/if}
