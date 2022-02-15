be3839eee293b019ea3d0e976400afc82b6fb82f
Andrei Marukhin <ax2mx@ya.ru>
2022-02-15 22:22:04 +0300

    Move key substitution logic to script section

--- src/sandbox/components/Shortcut.svelte
+++ src/sandbox/components/Shortcut.svelte
@@ -2,6 +2,24 @@
   import { isMac } from "../../utils/ui.js";
 
   export let title, keys = [];
+
+  function substituteKey(key) {
+    switch (key) {
+      case "ArrowDown":   return "↓";
+      case "ArrowUp":     return "↑";
+      case "ArrowLeft":   return "←";
+      case "ArrowRight":  return "→";
+      case "KeyC":        return "C";
+      case "KeyD":        return "D";
+      case "KeyV":        return "V";
+      case "KeyX":        return "X";
+      case "KeyZ":        return "Z";
+      case "Slash":       return "/";
+      case "Cmd":         return isMac() ? "⌘" : "Ctrl";
+      case "Alt":         return isMac() ? "⎇" : "Alt";
+      default:            return key;
+    }
+  }
 </script>
 
 <div class="flex flex-row content-center items-baseline justify-between gap-4">
@@ -12,19 +30,7 @@
       <span class="opacity-40">{key}</span>
     {:else}
       <kbd class="px-1 pt-px pb-0 opacity-60 border border-gray-900 border-opacity-20 text-xs rounded dark:border dark:border-white dark:border-opacity-20" >
-        {#if      key === "ArrowDown"}  {"↓"}
-        {:else if key === "ArrowUp"}    {"↑"}
-        {:else if key === "ArrowLeft"}  {"←"}
-        {:else if key === "ArrowRight"} {"→"}
-        {:else if key === "KeyC"}       {"C"}
-        {:else if key === "KeyD"}       {"D"}
-        {:else if key === "KeyV"}       {"V"}
-        {:else if key === "KeyX"}       {"X"}
-        {:else if key === "KeyZ"}       {"Z"}
-        {:else if key === "Slash"}      {"/"}
-        <!--{:else if key === "Cmd"}        {isMac() ? "⌘" : "Ctrl"}-->
-        <!--{:else if key === "Alt"}        {isMac() ? "⎇" : "Alt"}-->
-        {:else} {key} {/if}
+        {substituteKey(key)}
       </kbd>
     {/if}
   {/each}
