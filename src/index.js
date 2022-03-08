4bc9dcf644d38feacf7605738e07657da64b3a32
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-03-08 15:48:56 +0900

    remove accidentally committed file

--- src/index.js
+++
@@ -1,6 +0,0 @@
-export function createButton() {
-  const button = document.createElement('button');
-  button.innerText = 'Click me!!!';
-  button.classList.add('text-blue-700');
-  return button;
-}
